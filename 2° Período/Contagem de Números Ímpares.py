c1139f35b181fadf1de5e30697ea909a50bb4cb1
Andrey <andreyoliveira72005@gmail.com>
2025-10-18 19:34:32 -0300

    Delete 2° Período directory

--- 2° Período/Contagem de Números Ímpares.py
+++
@@ -1,8 +0,0 @@
-#8. Contagem de Números Ímpares 
-#Escreva um programa que conta e imprime todos os números ímpares entre 1 e 100.
-from time import sleep
-for c in range(1, 100):
-    if c % 2 != 0:
-        sleep(1)
-        print(c)
-    c += 1
